475f4e308c1e0db45973fbea64fa66eba3e3e390
Alessandro <alessandronobre.ti@gmail.com>
2024-08-11 10:56:35 -0300

    First commit

---
+++ src/main/java/com/devpro/msstudies/MsstudiesApplication.java
@@ -0,0 +1,13 @@
+package com.devpro.msstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsstudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsstudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpro/msstudies/MsstudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpro.msstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsstudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
